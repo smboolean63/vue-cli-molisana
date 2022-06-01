2a9dfa6680d80dc819fec189f77fd33018cb4e0f
smboolean63 <smboolean63@madrigali.dev>
2022-06-01 12:45:51 +0200

    Aggiunto header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <BaseHeader/>
   </div>
 </template>
 
 <script>
+import BaseHeader from './components/BaseHeader.vue';
+
 export default {
   name: 'App',
   components: {
+    BaseHeader,
   }
 }
 </script>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+      <div class="header__logo">
+          <img src="../assets/img/la-molisana-logo.png"/>
+      </div>
+      <nav class="header__nav">
+          <ul>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li class="active"><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/mixins.scss';
+
+.header {
+    text-align: center;
+
+    &__logo { // header__logo
+        margin-bottom: 1.25rem;
+    }
+
+    &__nav { // header__nav
+        ul { // header__nav ul
+            @include listInline;
+            justify-content: center;
+            a { // header__nav ul a
+                padding: 1.25rem;
+                text-decoration: none;
+                color: #000;
+            }
+            .active a { // header__nav ul .active a
+                background-color: var(--secondary-color);
+            }
+        }
+    }
+}
+</style>
