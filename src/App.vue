706988eb7db4779b0aa8e9efbca746585e9f7662
smboolean63 <smboolean63@madrigali.dev>
2022-06-01 12:52:47 +0200

    Aggiunto main

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <BaseHeader/>
+    <BaseMain/>
   </div>
 </template>
 
 <script>
 import BaseHeader from './components/BaseHeader.vue';
+import BaseMain from './components/BaseMain.vue';
 
 export default {
   name: 'App',
   components: {
     BaseHeader,
+    BaseMain,
   }
 }
 </script>
